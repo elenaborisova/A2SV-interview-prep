c0705e40b9e27149aa79edbdad9dff0384491ddf
Elena Borisova <elenaborrisova@gmail.com>
2022-02-09 19:59:43 +0100

    format code

--- contests/contest1/find_merge_node.py
+++ contests/contest1/find_merge_node.py
@@ -20,7 +20,7 @@ def findMergeNode(head1, head2):
         head2 = head2.next
 
 
-
+# Test cases:
 head1 = SinglyLinkedListNode(0)
 head1.next = SinglyLinkedListNode(1)
 node = SinglyLinkedListNode(2)
--- contests/contest1/super_digit.py
+++ contests/contest1/super_digit.py
@@ -1,18 +1,16 @@
 def superDigit(n, k):
-    # digit = n * k
-
     if len(n) == 1:
         return int(n)
 
     cur_sum = 0
     for d in n:
         cur_sum += int(d)
-
     cur_sum *= k
 
     return superDigit(str(cur_sum), 1)
 
 
+# Test cases:
 print(superDigit('123', 3))
 print(superDigit('148', 3))
 print(superDigit('9875', 4))
